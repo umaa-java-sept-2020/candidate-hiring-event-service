df518abbc5187b684098ad5e1f12516808e706ef
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-12-17 12:15:30 +0530

    added Address and Company Model

---
+++ src/main/java/com/uuv/candidatehiringeventservice/model/Address.java
@@ -0,0 +1,77 @@
+package com.uuv.candidatehiringeventservice.model;
+
+public class Address {
+
+    private Integer id;
+    private String uid;
+    private String firstLine;
+    private String lastLine;
+    private String pinCode;
+    private String state;
+    private String country;
+    private String landMark;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getFirstLine() {
+        return firstLine;
+    }
+
+    public void setFirstLine(String firstLine) {
+        this.firstLine = firstLine;
+    }
+
+    public String getLastLine() {
+        return lastLine;
+    }
+
+    public void setLastLine(String lastLine) {
+        this.lastLine = lastLine;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLandMark() {
+        return landMark;
+    }
+
+    public void setLandMark(String landMark) {
+        this.landMark = landMark;
+    }
+}
---
+++ src/main/java/com/uuv/candidatehiringeventservice/model/Company.java
@@ -0,0 +1,52 @@
+package com.uuv.candidatehiringeventservice.model;
+
+import java.util.List;
+
+public class Company {
+
+    private Integer id;
+    private String uid;
+    private String name;
+    private String displayName;
+    private List<Address> address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public List<Address> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<Address> address) {
+        this.address = address;
+    }
+}
