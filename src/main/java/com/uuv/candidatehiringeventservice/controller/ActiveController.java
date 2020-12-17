2688cc13b7db3bcd51bbb3a6659ba7c9e92b83eb
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-12-17 11:26:31 +0530

    added active controller

---
+++ src/main/java/com/uuv/candidatehiringeventservice/controller/ActiveController.java
@@ -0,0 +1,16 @@
+package com.uuv.candidatehiringeventservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class ActiveController {
+
+    @GetMapping("/active")
+    public String getActive()
+    {
+        return "I AM ACTIVE "+ new Date();
+    }
+}
