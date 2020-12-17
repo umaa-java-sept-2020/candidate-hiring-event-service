abba5e144fb3d7772871ee9cc1a15b4138b355da
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-12-17 11:17:20 +0530

    initial commit

---
+++ src/main/java/com/uuv/candidatehiringeventservice/CandidateHiringEventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uuv.candidatehiringeventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateHiringEventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateHiringEventServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uuv/candidatehiringeventservice/CandidateHiringEventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uuv.candidatehiringeventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateHiringEventServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
