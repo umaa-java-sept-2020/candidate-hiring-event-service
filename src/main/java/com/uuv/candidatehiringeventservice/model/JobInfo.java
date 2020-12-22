28590ad3f974ac591047f0f159575fb155e43173
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-12-22 17:19:55 +0530

    job level and jobInfo added

---
+++ src/main/java/com/uuv/candidatehiringeventservice/model/JobInfo.java
@@ -0,0 +1,88 @@
+package com.uuv.candidatehiringeventservice.model;
+
+import java.util.Date;
+
+public class JobInfo {
+
+    private Integer id;
+    private String uid;
+    private String title;
+    private String description;
+    private JobLevel jobLevel;
+    private Department department;
+    private Company company;
+    private Address address;
+    private Date postedOn;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public JobLevel getJobLevel() {
+        return jobLevel;
+    }
+
+    public void setJobLevel(JobLevel jobLevel) {
+        this.jobLevel = jobLevel;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Date getPostedOn() {
+        return postedOn;
+    }
+
+    public void setPostedOn(Date postedOn) {
+        this.postedOn = postedOn;
+    }
+}
---
+++ src/main/java/com/uuv/candidatehiringeventservice/model/JobLevel.java
@@ -0,0 +1,5 @@
+package com.uuv.candidatehiringeventservice.model;
+
+public enum JobLevel {
+    L1, L2, L3, L4, L5;
+}
