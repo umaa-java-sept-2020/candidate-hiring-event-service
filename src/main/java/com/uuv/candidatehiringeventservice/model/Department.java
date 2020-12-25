9bdd5b5d10ca5d20574225966f17bb8e9af3e667
swopnalika mohapatra <swopnalikamohapatra185@gmail.com>
2020-12-25 20:14:07 +0530

    added department pojo

---
+++ src/main/java/com/uuv/candidatehiringeventservice/model/Department.java
@@ -0,0 +1,50 @@
+package com.uuv.candidatehiringeventservice.model;
+
+public class Department {
+    private int id;
+    private String uid;
+    private String name;
+    private String displayName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Department(int id, String uid, String name, String displayName) {
+        this.id = id;
+        this.uid = uid;
+        this.name = name;
+        this.displayName = displayName;
+    }
+
+    public Department() {
+    }
+}
